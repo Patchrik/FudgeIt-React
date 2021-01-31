9027be442d43fda88c39f6828c62834012376dd0
Patchrik <pklly92@gmail.com>
2021-01-31 09:11:24 -0600

    added the first part of the tag repo

---
+++ Fudge-It/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using Fudge_It.Models;
+using System.Collections.Generic;
+
+namespace Fudge_It.Repositories
+{
+    public interface ITagRepository
+    {
+        List<Tag> GetTagsByUserProfileId(int userProfileId);
+
+        void Add(Tag tag);
+    }
+}
---
+++ Fudge-It/Repositories/TagRepository.cs
@@ -0,0 +1,32 @@
+﻿using Fudge_It.Data;
+using Fudge_It.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fudge_It.Repositories
+{
+    public class TagRepository : ITagRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Tag> GetTagsByUserProfileId(int userProfileId)
+        {
+            return _context.Tag.Where(tag => tag.UserProfileId == userProfileId).ToList();
+
+        }
+
+        public void Add(Tag tag)
+        {
+            _context.Add(tag);
+            _context.SaveChanges();
+        }
+    }
+}
