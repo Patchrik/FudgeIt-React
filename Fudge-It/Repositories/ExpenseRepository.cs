b2f5b1c800589197e56edd869777d7b47b840064
Patchrik <pklly92@gmail.com>
2021-01-31 08:46:25 -0600

    added in the first bit of the expense repo

---
+++ Fudge-It/Repositories/ExpenseRepository.cs
@@ -0,0 +1,32 @@
+﻿using Fudge_It.Data;
+using Fudge_It.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fudge_It.Repositories
+{
+    public class ExpenseRepository : IExpenseRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Expense> GetExpensesByUserProfileId(int userProfileId)
+        {
+            return _context.Expense.Where(up => up.UserProfileId == userProfileId).ToList();
+
+        }
+
+        public void Add(Expense expense)
+        {
+            _context.Add(expense);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Fudge-It/Repositories/IExpenseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Fudge_It.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fudge_It.Repositories
+{
+    public interface IExpenseRepository
+    {
+        void Add(Expense expense);
+        List<Expense> GetExpensesByUserProfileId(int userProfileId);
+    }
+}
