9967a4dcbb5dad4c8785409f7f43f2e5d0fcab5a
Patchrik <pklly92@gmail.com>
2021-01-31 09:31:11 -0600

    added the ExpenseTag repos. Looks like we're done with the repos so far.

---
+++ Fudge-It/Repositories/ExpenseTagRepository .cs
@@ -0,0 +1,32 @@
+﻿using Fudge_It.Data;
+using Fudge_It.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fudge_It.Repositories
+{
+    public class ExpenseTagRepository : IExpenseTagRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExpenseTagRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<ExpenseTag> GetExpenseTagsByExpenseId(int expenseId)
+        {
+            return _context.ExpenseTag.Where(exTag => exTag.ExpenseId == expenseId).ToList();
+
+        }
+
+        public void Add(ExpenseTag exTag)
+        {
+            _context.Add(exTag);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Fudge-It/Repositories/IExpenseTagRepository.cs
@@ -0,0 +1,13 @@
+﻿using Fudge_It.Models;
+using System.Collections.Generic;
+
+namespace Fudge_It.Repositories
+{
+    public interface IExpenseTagRepository
+    {
+        List<ExpenseTag> GetExpenseTagsByExpenseId(int expenseId);
+
+        void Add(ExpenseTag exTag);
+
+    }
+}
